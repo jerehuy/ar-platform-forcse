e0ab6e2166a969f7b95384ab18e39c6619be2a76
Camilla Selkälä <camilla.selkala@tuni.fi>
2020-10-06 11:23:27 +0300

    Different Images show different prefabs

---
+++ Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageTracking : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] placeablePrefabs;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+    private ARTrackedImageManager trackedImageManager;
+
+    private void Awake()
+    {
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+        foreach(GameObject prefab in placeablePrefabs)
+        {
+            GameObject uusiPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            uusiPrefab.name = prefab.name;
+            spawnedPrefabs.Add(prefab.name, uusiPrefab);
+        }
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach(ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        string nimi = trackedImage.referenceImage.name;
+        Vector3 paikka = trackedImage.transform.position;
+
+        GameObject prefab = spawnedPrefabs[nimi];
+        prefab.transform.position = paikka;
+        prefab.SetActive(true);
+
+        foreach(GameObject go in spawnedPrefabs.Values)
+        {
+            if(go.name != nimi)
+            {
+                go.SetActive(false);
+            }
+        }
+    }
+}
