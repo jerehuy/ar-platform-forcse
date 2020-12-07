3028f0dd8e943ded707322004fee8851011a844a
Jaakko <jaakko.huotari@tuni.fi>
2020-12-07 19:56:25 +0200

    Added a hard coded audiomanager and a connection to the play button. Fixes to audio control panel

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+// Kovakoodattu audiomanageri!!! Täytyy muokata rajusti
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    private int currentClip;
+    private AudioSource source;
+
+    void Start ()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if(source.isPlaying)
+        {
+            return;
+        }
+
+        currentClip--;
+        if (currentClip < 0)
+        {
+            currentClip = audioClips.Length - 1;
+        }
+
+        StartCoroutine(WaitForMusicEnd());
+    }
+
+    IEnumerator WaitForMusicEnd()
+    {
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+        NextClip();
+    }
+
+    public void NextClip()
+    {
+        source.Stop();
+        currentClip++;
+
+        if (currentClip > audioClips.Length - 1)
+        {
+            currentClip = 0;
+        }
+        source.clip = audioClips[currentClip];
+        source.Play();
+
+        StartCoroutine(WaitForMusicEnd());
+    }
+
+    public void StopMusic()
+    {
+        StopCoroutine("WaitForMusicEnd");
+        source.Stop();
+    }
+}
